7fe602ab85dc1752a8dbc86a8239ee32b13b44fb
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-12-20 01:42:57 +0530

    DONE

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,42 @@
-import { useState } from "react";
-
-const sizes = {
-  small: "w-40 h-40",
-  large: "w-72 h-72",
-};
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const [size, setSize] = useState("small");
+ 
+  const[size, setSize] = useState("small");
 
-  return (
-    <div className="min-h-screen flex items-center justify-center gap-6  bg-amber-900">
-      <div
-        className={`bg-blue-600 text-white flex items-center justify-center
-          ${sizes[size]} transition-all duration-700`}
-      >
-        Card
-      </div>
-
-      <button
-        onClick={() =>
-          setSize(size === "small" ? "large" : "small")
-        }
-        className="px-4 py-2 border"
-      >
-        Toggle Size
-      </button>
-    </div>
-  );
+ const sizes = {
+  small: "w-40 h-40",
+  large: "w-80 h-80",
 };
 
-export default App;
+useEffect(() => {
+  const savedSize = localStorage.getItem("size");
+ 
+  if(savedSize) {
+    setSize(savedSize);
+  };
+
+},[]);
+
+useEffect(() => {
+ localStorage.setItem("size", size);
+},[size]);
+
+  return (
+    <>
+    <div className='min-h-screen bg-gray-900 flex justify-center items-center gap-7'>
+      <div
+      className={`text-white bg-red-300 ${sizes[size]} 
+      transition-all duration-100  `}>Div</div>
+      <button
+      onClick={() => 
+        setSize(size === "small"? "large": "small")
+      }
+      className='text-white border rounded px-1 py-1 cursor-pointer hover:scale-110 transition-transform'
+      >Toggle</button>
+    </div>
+    </>
+  )
+}
+
+export default App
